b71a47fb756c47bcd14e72cc64968fcaaa228097
Arief <rhmnarief7@gmail.com>
2022-05-14 13:47:53 +0700

    add models, routes and controller for blog

--- index.js
+++ index.js
@@ -1,9 +1,60 @@
 const express = require('express');
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const multer = require('multer')
+const path = require('path')
 
 const app = express();
+const blogRoutes = require('./src/routes/blog')
 
-app.use(()=>{
-    console.ino('Hello Server!')
+const fileStorage = multer.diskStorage({
+    destination : (req, file, cb) => {
+        cb(null, 'images')
+    },
+    filename: (req, file, cb) => {
+        cb(null, new Date().getTime() + '-' + file.originalname)
+    }
 })
 
-app.listen(4000)
+const fileFilter = (req, res, cb) => {
+    if(
+        file.mimetype === 'image/png' ||
+        file.mimetype === 'image/jpg' ||
+        file.mimetype === 'image/jpeg'
+    ){
+        cb(null, true)
+    }else{
+        cb(null, false)
+    }
+}
+
+app.use(bodyParser.json())
+app.use('/images', express.static(path.join(__dirname, 'images')))
+app.use(multer({storage: fileStorage, fileFilter: fileFilter}).single('image'))
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next()
+})
+
+app.use('/v1/blog', blogRoutes)
+
+app.use((error, req, res, next) =>{
+    const status = error.errorStatus || 500
+    const message = error.message
+    const data = error.data
+
+    res.status(status).json(
+        {
+            message: message,
+            data: data
+        }
+    )
+})
+mongoose.connect('mongodb://localhost:27017')
+.then(() => {
+    app.listen(4000, () => console.log('Connection Succesfully'))
+})
+.catch(err => console.log(err))
